dafdcffc92c50ad15c44c1d429a44fbe8d5345c5
Abboud21 <abboudmucy@gmail.com>
2025-11-19 10:16:10 -0300

    Detalhe das Noticias

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from noticias.views import index, autores, noticias, buscar
+from noticias.views import index, autores, noticias, buscar, detalhe_noticia
 
 urlpatterns = [
     path('',noticias,name='index'),
     path('autor/',autores),
     path('buscar',buscar,name='buscar'),
+    path('<int:noticia_id>/',detalhe_noticia,name='detalhe_noticia'),
 ]
